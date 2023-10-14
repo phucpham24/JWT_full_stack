d218c89fe839288fd2f6c641ae54563d69abb8b7
phuc pham <phamtrandaiphuchcmut@gmail.com>
2023-10-14 22:08:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.scss";
+import Nav from "./components/Navigation/Nav";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login/login";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world React with Hoi Dan IT
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        {/* <Nav /> */}
+        <Switch>
+          <Route path="/news">News</Route>
+          <Route path="/about">About</Route>
+          <Route path="/contact">Contact</Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/" exact>
+            Home
+          </Route>
+          <Route path="*">404 not found</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Login/login.js
@@ -0,0 +1,15 @@
+import "./login.scss";
+
+const Login = (props) => {
+  return (
+    <div className="login-container">
+      <div className="container">
+        <div className="row">
+          <div className="content-left">left</div>
+          <div className="content-right">right</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Login;
